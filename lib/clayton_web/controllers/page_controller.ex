9cc109e557c58eee492c4f4d1f9acc66e1406801
Manfred Wuits <manfred@monochrom.at>
2020-05-27 22:42:25 +0200

    upload

--- lib/clayton_web/controllers/page_controller.ex
+++ lib/clayton_web/controllers/page_controller.ex
@@ -9,8 +9,54 @@ defmodule ClaytonWeb.PageController do
     conn |> html("URL is ok")
   end
 
-  def store(conn, params) do
-    conn |>IO.inspect(label: "PUT mwuits-debug 2020-05-27_21:55 ")
-    render(conn, "index.html")
+
+  def store(conn, %{"upload" => %Plug.Upload{} = upload, "type" => type}) do
+    case create_upload_from_plug_upload(type, upload) do
+      {:ok, upload} ->
+         conn  |> html("ok #{inspect (upload)}")
+        # redirect(conn, to: Routes.upload_path(conn, :index, type))
+
+      {:error, reason} ->
+        conn  |> html( "error upload file: #{inspect(reason)}")
+        # render(conn, "new.html")
+    end
   end
+
+  def create_upload_from_plug_upload(type, %Plug.Upload{
+    filename: filename,
+    path: tmp_path,
+    content_type: content_type
+  })
+  when is_binary(type) do
+hash =
+  File.stream!(tmp_path, [], 2048)
+  |> Upload.sha256()
+
+  with {:ok, %File.Stat{size: size}} <- File.stat(tmp_path),
+           {:ok, upload} <-
+             %Upload{}
+             |> Upload.changeset(%{
+               filename: filename,
+               content_type: content_type,
+               hash: hash,
+               size: size,
+               type: type
+             })
+             |> Repo.insert(),
+           :ok <-
+             File.cp(
+               tmp_path,
+               Upload.local_path(upload.id, upload.type, filename)
+               |> IO.inspect(label: "mwuits-debug 2020-01-13_17:41 ")
+             ) do
+        {:ok, upload}
+      else
+        {:error, reason} -> Repo.rollback(reason)
+      end
+end)
+
+# upload creation logic
+end
+
+
 end
